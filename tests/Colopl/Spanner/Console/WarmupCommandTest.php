92d3f57f9370dbedc8025427f4f2f2d7b65ab8bd
Taka Oyama <t-oyama@colopl.co.jp>
2022-12-02 11:49:31 +0900

    clean up tests

--- tests/Colopl/Spanner/Console/WarmupCommandTest.php
+++ tests/Colopl/Spanner/Console/WarmupCommandTest.php
@@ -18,8 +18,6 @@
 namespace Colopl\Spanner\Tests\Console;
 
 use Colopl\Spanner\Connection;
-use Colopl\Spanner\Tests\TestCase;
-use Illuminate\Testing\PendingCommand;
 
 class WarmupCommandTest extends TestCase
 {
@@ -31,23 +29,33 @@ class WarmupCommandTest extends TestCase
             $this->setUpDatabaseOnce($conn);
         }
 
-        $command = $this->artisan('spanner:warmup');
-        assert($command instanceof PendingCommand);
-
         // Emulator cannot distinguish different connections
         $altWarmupCount = env('SPANNER_EMULATOR_HOST') ? 0 : 1;
 
-        $command
+        $this->artisan('spanner:warmup')
             ->expectsOutputToContain("Warmed up 1 sessions for main")
             ->expectsOutputToContain("Warmed up {$altWarmupCount} sessions for alternative")
             ->assertSuccessful()
             ->run();
 
-        $command2 = $this->artisan('spanner:warmup');
-        assert($command2 instanceof PendingCommand);
-        $command2
+        $this->artisan('spanner:warmup')
             ->expectsOutputToContain("Warmed up 0 sessions for main")
             ->expectsOutputToContain("Warmed up 0 sessions for alternative")
+            ->assertSuccessful()
+            ->run();
+    }
+
+    public function test_with_args(): void
+    {
+        foreach (['main', 'alternative'] as $name) {
+            $conn = $this->getConnection($name);
+            assert($conn instanceof Connection);
+            $this->setUpDatabaseOnce($conn);
+        }
+
+        $this->artisan('spanner:warmup', ['connections' => 'main'])
+            ->expectsOutputToContain("Warmed up 1 sessions for main")
+            ->doesntExpectOutputToContain("alternative")
             ->assertSuccessful()
             ->run();
     }
