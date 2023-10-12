b57fa3df426d5b1c06b19e0d9430652407a12fc9
Takayasu Oyama <toyama4649@hotmail.com>
2023-10-12 09:04:33 +0900

    feat: allow custom cache path (#142)
    * feat: allow custom cache path
    
    * add changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,7 @@ Added
 - Deprecation warnings to `Connection`'s methods `cursorWithTimestampBound` `selectWithTimestampBound` `selectOneWithTimestampBound`. Use `cursorWithOptions` `selectWithOptions` instead. (#122)
 - `Connection` has new methods `selectWithOptions` `cursorWithOptions` which allows spanner specific options to be set for each query. (#122)
 - `session:list` command can now show and filter by labels. (#134)
+- Allow custom cache path (#142)
 
 Changed
 - [Breaking] Match `Query\Builder::forceIndex()` behavior with laravel's (`forceIndex` property no longer exists). (#114)
---
+++ tests/SpannerServiceProviderTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Copyright 2019 Colopl Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Colopl\Spanner\Tests;
+
+use Illuminate\Database\DatabaseManager;
+
+class SpannerServiceProviderTest extends TestCase
+{
+    public function test_change_cache_path(): void
+    {
+        $newPath = '/tmp/spanner';
+        config()->set('database.connections.main.cache_path', $newPath);
+        $this->beforeApplicationDestroyed(static fn () => shell_exec('rm -rf {$newPath}'));
+
+        /** @var DatabaseManager $db */
+        $db = $this->app->make('db');
+
+        $db->connection('main')->query()->select('SELECT 1');
+
+        $this->assertDirectoryExists($newPath);
+    }
+}
