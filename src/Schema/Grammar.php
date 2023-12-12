b7e5282bed0436e4b302582b46998666b412721d
Matthew <matthewjumpsoffbuildings@gmail.com>
2023-12-12 12:50:49 +1030

    Update src/Schema/Grammar.php
    Co-authored-by: Takayasu Oyama <taka.oyama@gmail.com>

--- src/Schema/Grammar.php
+++ src/Schema/Grammar.php
@@ -417,7 +417,7 @@ class Grammar extends BaseGrammar
     /**
      * Create the column definition for a long text type.
      *
-     * @param  \Illuminate\Support\Fluent  $column
+     * @param  Fluent<string, mixed> $column
      * @return string
      */
     protected function typeLongText(Fluent $column)
