b3fe074699f4e12a76d288205356fa5f7479bc08
Matthew <matthewjumpsoffbuildings@gmail.com>
2025-02-25 11:51:32 +1030

    Update SpannerJson.php

--- src/Casts/SpannerJson.php
+++ src/Casts/SpannerJson.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace Colopl\Spanner\Casts;
+
+use Google\Cloud\Spanner\PgJsonb;
+use Google\Cloud\Spanner\V1\TypeAnnotationCode;
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+class SpannerJson implements CastsAttributes
+{
+	public function get($model, $key, $value, $attributes)
+	{
+		return json_decode((string) $value, true);
+	}
+
+	public function set($model, $key, $value, $attributes)
+	{
+		return [$key => new SpannerJsonType($value)];
+	}
+}
+
+class SpannerJsonType extends PgJsonb
+{
+	public function typeAnnotation()
+	{
+		return TypeAnnotationCode::TYPE_ANNOTATION_CODE_UNSPECIFIED;
+	}
+}
