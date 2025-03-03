ec6efc89421975afd35c982d2963a5208151ed24
Matt <matthewjumpsoffbuildings@gmail.com>
2025-03-03 15:30:37 +1030

    Update SpannerJson.php

--- src/Casts/SpannerJson.php
+++ src/Casts/SpannerJson.php
@@ -6,7 +6,6 @@ use Google\Cloud\Spanner\PgJsonb;
 use Google\Cloud\Spanner\V1\TypeAnnotationCode;
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 use Illuminate\Database\Eloquent\Model;
-use JsonSerializable;
 
 /** 
  * @implements CastsAttributes<mixed, mixed> 
@@ -15,16 +14,16 @@ class SpannerJson implements CastsAttributes
 {
     public function get(Model $model, string $key, mixed $value, array $attributes)
     {
-        if ($value === null) {
+        if($value === null) {
             return null;
         }
 
-        if (is_array($value)) {
-            return $value;
+        if($value instanceof SpannerJsonType) {
+            $value = (string) $value;
         }
 
-        if (!is_string($value)) {
-            throw new \InvalidArgumentException('The given value must be an array, string or null.');
+        if(!is_string($value)) {
+            throw new \InvalidArgumentException('The given value must be a string, SpannerJsonType or null');
         }
 
         return json_decode($value, true);
@@ -34,7 +33,7 @@ class SpannerJson implements CastsAttributes
     {
         if (
             !is_array($value) && 
-            !$value instanceof JsonSerializable && 
+            !$value instanceof \JsonSerializable && 
             $value !== null && 
             !is_string($value)
         ) {
