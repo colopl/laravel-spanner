43a066232fc3f7a1383cc988950be5bc9dddaa63
Matthew <matthewjumpsoffbuildings@gmail.com>
2025-03-13 13:26:45 +1030

    Update SnapshotTest.php

--- tests/SnapshotTest.php
+++ tests/SnapshotTest.php
@@ -46,23 +46,6 @@ class SnapshotTest extends TestCase
         $this->assertSame('ok', $result);
     }
 
-    public function test_builder_snapshot(): void
-    {
-        $conn = $this->getDefaultConnection();
-
-        $conn->transaction(function () use ($conn) {
-            $this->assertFalse($conn->inSnapshot());
-            $conn->table(self::TABLE_NAME_USER)->insert(['userId' => $this->generateUuid(), 'name' => 't']);
-        });
-
-        $this->assertFalse($conn->inSnapshot());
-        $result = $conn->table(self::TABLE_NAME_USER)->snapshot(new StrongRead())->first();
-
-        $this->assertFalse($conn->inSnapshot());
-        $this->assertNotNull($result);
-        $this->assertSame('t', $result['name']);
-    }
-
     public function test_snapshot_with_staleness(): void
     {
         $conn = $this->getDefaultConnection();
