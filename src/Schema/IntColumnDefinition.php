8c588c92e33b6b82c2013be0b11b8afdb4e5fa40
Takayasu Oyama <t-oyama@colopl.co.jp>
2025-05-09 12:58:58 +0900

    feat: allow auto increment to be set through Blueprint

--- src/Schema/IntColumnDefinition.php
+++ src/Schema/IntColumnDefinition.php
@@ -20,8 +20,6 @@ declare(strict_types=1);
 
 namespace Colopl\Spanner\Schema;
 
-use Illuminate\Database\Schema\ColumnDefinition;
-
 /**
  * @property string $name
  * @property string|null $useSequence
