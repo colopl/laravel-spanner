f2a139d113fb66c7c604ce7b752084055091e791
Takayasu Oyama <t-oyama@colopl.co.jp>
2025-05-14 11:32:32 +0900

    fix type

--- src/Query/Processor.php
+++ src/Query/Processor.php
@@ -74,7 +74,7 @@ class Processor extends BaseProcessor
      *
      * {@inheritDoc}
      * @param list<array<string, mixed>> $results
-     * @return list<array{name: string, type: string, type_name: string, nullable: bool, collation: string|null, default: mixed, auto_increment: bool, comment: string|null, generation: array{type: string, expression: string|null}|null}>
+     * @return list<array{name: string, type: string, type_name: string, nullable: bool, collation: null, default: mixed, auto_increment: bool, comment: null, generation: null|null}>
      */
     public function processColumns($results)
     {
