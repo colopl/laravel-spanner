36bb70e761a6f30b9155d20427c8f2e40daab657
Takayasu Oyama <t-oyama@colopl.co.jp>
2025-07-02 16:56:22 +0900

    feat: add toggle to disableEmulatorNullFilteredIndexCheck for ease of use

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -268,9 +268,10 @@ class Builder extends BaseBuilder
     }
 
     /**
+     * @param bool $toggle
      * @return $this
      */
-    public function disableEmulatorNullFilteredIndexCheck(): static
+    public function disableEmulatorNullFilteredIndexCheck(bool $toggle = true): static
     {
         $indexHint = $this->indexHint;
 
@@ -279,7 +280,7 @@ class Builder extends BaseBuilder
         }
 
         assert($indexHint instanceof IndexHint);
-        $indexHint->disableEmulatorNullFilteredIndexCheck = true;
+        $indexHint->disableEmulatorNullFilteredIndexCheck = $toggle;
 
         return $this;
     }
